c226f3e286fef4f5c54d12a868b12b3b8f892216
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-12-12 20:18:45 -0500

    chore: init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Home Page</h1>
+      <a href="/contact">Contact Us</a>
+    </>
+  )
+}
+
+export default App
---
+++ src/contact.tsx
@@ -0,0 +1,33 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import './contact.css'
+
+function Contact() {
+  return (
+    <>
+      <h1>Contact Us</h1>
+      <form>
+        <input type="text" placeholder="Name" />
+        <input type="email" placeholder="Email" />
+        <input type="text" placeholder="Message" />
+        <button type="submit">Submit</button>
+      </form>
+      <button
+        type="button"
+        className="secondary"
+        onClick={() => {
+          window.location.href = '/'
+        }}
+      >
+        Return home
+      </button>
+    </>
+  )
+}
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Contact />
+  </StrictMode>,
+)
